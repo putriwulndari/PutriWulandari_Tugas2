0eb766e6a5ae6dd9702ad5069a318b0049658f7b
putriwulndari <118438402+putriwulndari@users.noreply.github.com>
2023-06-20 23:13:32 +0700

    Initial commit

---
+++ app/views/index/index.php
@@ -0,0 +1,11 @@
+<h2>Log In</h2>
+
+<div class="info">Selamat datang, <strong>PUTRI</strong></div>
+
+<form action="<?php echo URL; ?>Dashboard/index.php" method="post">
+    <p>username</p>
+    <input type="text">
+    <p>password</p>
+    <input type="password">
+    <input type="submit" value="Login">
+</form>
